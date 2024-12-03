28dacdc6147bcb893a2ac9f0ab5901472c5a1b48
weijianan1 <148740075+weijianan1@users.noreply.github.com>
2024-12-03 11:11:31 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,5 +4,5 @@ CUDA_VISIBLE_DEVICES=0,1 python difftr_flow3d_main.py --train_dataset TR-Flow3D 
 CUDA_VISIBLE_DEVICES=3 python difftr_flow3d_main.py --train_dataset TR-Flow3D --val_dataset TR-Flow3D --lr 4e-5 --train_batch_size 8 --test_batch_size 4 --num_epochs 500 \
         --result_dir results \
         --resume model_best.pt \
-         --eval
+        --eval
 
